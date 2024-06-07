7ef08ba57c8cac9c54a54316c69391699567a353
tzy <tangzy2021@gmail.com>
2024-06-07 11:48:00 +0800

    change docker file

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.send('欢迎使用微信云托管！');
 });
 
 router.get('/hello', function (req, res, next) {
